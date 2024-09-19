64102355f24ac6f2d74807e0fa93c20e53beffe5
Guilherme Leite <guiroleite@gmail.com>
2024-09-18 21:01:24 -0300

    toma

---
+++ scriptForms.js
@@ -0,0 +1,13 @@
+let idade = 20
+let nome
+
+alert("Oi gente")
+
+nome = prompt("Digite seu nome ooo infeliz!!!!")
+idade = prompt("Digite sua idade")
+idade = parseInt(idade)
+console.log("Meu nome é " + nome)
+console.log("Tenho " + idade + " anos")
+console.log("vou fazer " + (idade + 1) + " anos")
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
+
---
+++ scriptNotas.js
@@ -0,0 +1,6 @@
+let nota1, nota2, media
+
+nota1 = parseInt(prompt("Digite Nota 1"))
+nota2 = parseInt(prompt("Digite Nota 2"))
+media = (nota1 + nota2) / 2
+console.log(`sua média é ${media}`)
